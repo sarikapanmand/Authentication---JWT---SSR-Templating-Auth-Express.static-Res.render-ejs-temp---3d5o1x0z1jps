3e6a55a39680fd0eae675f4257e78e8d106fefd1
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2022-11-07 13:39:36 +0530

    Update validation.js

--- src/routes/validation.js
+++ src/routes/validation.js
@@ -4,10 +4,23 @@ const Joi = require("joi");
 // Register validation
 const registerValidation = (data) => {
   // Your code goes here
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  });
+
+  return schema.validate(data);
 };
 // Login validation
 const loginValidation = (data) => {
   // Your code goes here
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  });
+
+  return schema.validate(data);
 };
 
 module.exports.registerValidation = registerValidation;
