0e395f5866bf30ff1179fab92c95077cc78c02f3
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2022-11-07 13:36:43 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,12 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
   // Your code goes here
-})
+  name: String,
+  email: String,
+  password: String,
+});
 
-const User = mongoose.model('user',userSchema);
+const User = mongoose.model("user", userSchema);
 
 module.exports = User;
