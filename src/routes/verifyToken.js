31ccda4cc1f8ede243da0e765fc13145c961b98f
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2022-11-07 13:40:16 +0530

    Update verifyToken.js

--- src/routes/verifyToken.js
+++ src/routes/verifyToken.js
@@ -1,11 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-function verifyAuthToken (req, res, next) {
-    // Your code goes here
-}   
+function verifyAuthToken(req, res, next) {
+  const authToken = req.header("auth-token");
+  if (!authToken) {
+    res.status(401).send({ message: "Access denied" });
+  }
+  next();
+}
 
-function verifyRefreshToken (req, res, next) {
-    // Your code goes here
+function verifyRefreshToken(req, res, next) {
+  const refreshToken = req.header("refresh-token");
+  if (!refreshToken) {
+    res.status(401).send({ message: "Access denied" });
+  }
+  next();
 }
 
 module.exports.verifyAuthToken = verifyAuthToken;
